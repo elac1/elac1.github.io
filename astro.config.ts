b9e435348abccbca2a3898bbef5f58aa784a6386
ttlcl <2797669050@qq.com>
2026-09-08 20:45:11 +0800

    配置完成,准备部署

--- astro.config.ts
+++ astro.config.ts
@@ -16,7 +16,7 @@ import { imageConfig } from './src/utils/image-config'
 import path from 'path'
 
 export default defineConfig({
-  site: themeConfig.site.website,
+  site: 'https://elac1.github.io',,
   image: {
     service: {
       entrypoint: 'astro/assets/services/sharp',
